1c094cd2b31f8af4058518cc243f08995f1c7cee
Rexofoam <jonathanfoong1997@gmail.com>
2020-05-26 15:27:16 +0800

    added dashboard template

---
+++ login_module/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+	include 'DBconnect.php';
+  	session_start();
+
+  	//connection to database
+  	$con = DatabaseConn();
+
+  	// Get user login time
+  	$date = new DateTime();
+	$date->add(new DateInterval('PT06H'));
+	$AccessTime = $date->format('Y-m-d H:i:s');
+
+	
+    $Email = $_POST["email"]; 
+    $password = sha1($_POST["password"]);
+    
+
+	$sql = mysqli_query($con,"SELECT * FROM users WHERE email = '$Email' AND password = '$password'");
+
+	$result = mysqli_fetch_array($sql);
+	
+	if($result == null) {
+		$_SESSION["Credential_text"] = "Incorrect Username or Password !!!";
+    	header("Location: index.php");
+	}
+	// TODO: remove hard code
+	if($result['id'] == 1) {
+		header("Location: ../dashboard_module/dashboard.html");
+	}
+	else {
+		
+	}
+
+?>
+
+</body>
+</html>
